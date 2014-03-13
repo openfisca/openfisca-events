259260742bb95e87023de7d96444cc9ca33c5523
Guillot <malkaguillot@gmail.com>
2014-03-13 18:51:38 +0100

    R programs for using OF API

---
+++ R/R_API_celib.R
@@ -0,0 +1,59 @@
+####
+### API needs JSON format input and gives JSON outputs
+### This program :
+# 1. converts an input data to a json file
+# 2. get the results from the API and converts them to R data the outputs
+
+
+#####   0. Preparation  #####
+# Warning : order matters in packages loading
+library(rjson)
+library(jsonlite)
+library(httr)
+setwd("my_repository")
+source("R_json.R")
+
+##### 1. R to JSON ####
+## from your dataframe to API's input file
+	# all possible output variables are in model.py
+	# or available from this link : 
+	# http://nbviewer.ipython.org/github/openfisca/openfisca-web-notebook/blob/master/liste-des-variables.ipynb
+
+## 1.1 Scenario / Input variables ##
+
+# In this example we use the following case
+# Celibataire sans enfant with 4 INDIVIDUAL variables
+var <-  4
+data <- array(NA, dim=c(var,3))
+data[1,] <-  c("cadre", "true", 0)			# or "false"
+data[2,] <-  c("activite", "Actif occupé", 1)
+data[3,] <-  c("birth", "1970", 0)			# birth year
+data[4,] <-  c("sali", "20000", 0)		# taxable income 
+# O if boolean of numerical values // 1 if character
+
+## 1.2 Decomposition / Choice of output variables ##
+#  This example gives as output :
+## 'salsuperbrut' 	# salaire superbrut
+## 'salnet'			# salaire net
+## 'sali'			# salaire imposable
+## 'loyer'			# loyer
+## 'revdisp'		# revenu disponible
+
+## FINAL INPUT 
+json_input <- R_to_json(data)
+
+##### 2. CALLING THE API (website calculator) ####
+## Get the results : Output = Openfisca(Input)
+## json output file is converted to R dataframe object
+result <- json_to_R(json_input)
+
+
+
+fromJSON(result_celib)
+response = content(result_celib)
+str(response)
+as.data.frame(response$value)
+
+
+### 4. EXAMPLES
+
---
+++ R/R_json.R
@@ -0,0 +1,69 @@
+## This program defines functions to convert a openfisca input data into json format
+
+# auxiliary functions to work with characters chain
+# le probleme etant d'ecrire des " dans des chaines avec la fonction paste()
+guillemets <- function(x){ noquote(paste("",x, "", sep='"')) } 
+
+## Parties inchangees
+	chaine <- "[{"
+	# debut de la chaine
+	chaine_deb <- '{"scenarios": 
+		[{ 
+		"test_case": {
+			"foyers_fiscaux": 
+				[{"declarants": ["ind0"]}], 
+			"individus":' 
+
+	# fin de la chaine
+	chaine_fin <- ', "statmarit": "Célibataire", "id": "ind0" }],
+				"familles": [{"parents": ["ind0"]}], 
+				"menages": [{"personne_de_reference": "ind0"}]
+					}, 
+		"year": 2013}],
+	"decomposition": 
+		[{"code": "salsuperbrut", "title": "Salaire super brut"}, 
+		 {"code": "salnet", "title": "Salaire net"}, 
+		 {"code": "sali", "title": "Salaire imposable"}, 
+		 {"code": "loyer", "title": "Loyer"}, 
+		 {"code": "revdisp", "title": "Revenu disponible"}]
+	}'
+
+R_to_json <- function(data) {
+	# liste des variables entre guillemets
+	names_var <- guillemets(data[,1])
+	# distinction entre variables nombres ou characteres
+	index <- data[,3]
+	# application des guillemets si besoin
+	edit_values <- guillemets(data[data[,3]==1][2])
+	edit_data <- array(NA, dim=c(var,2))
+	edit_data[,1] <-  names_var
+	edit_data[index==1,2] <-  edit_values 
+	edit_data[index==0,2] <- data[index==0,2]
+
+	for (i in 1:(var-1)) {
+		chaine <- paste(chaine,paste(edit_data[i,1], ": ",edit_data[i,2], ", ", sep = ""))
+	}
+	chaine <- paste(chaine,paste(edit_data[var,1], ": ",edit_data[var,2], sep = ""))
+	noquote(chaine)
+	j_out <- noquote(paste(chaine_deb, chaine, chaine_fin, sep=""))
+	return(j_out)
+}
+
+json_to_R <- function(input){
+	result_sim <-  POST(url='http://api.openfisca.fr/api/1/simulate', body = json_input,
+          add_headers('Content-Type'= 'application/json',
+                      'User-Agent'='OpenFisca-Notebook'))
+	res <- content(result_sim)
+	#str(res) #the R-structure of the previous object
+	df_out<-data.frame(res$value)
+	df_final <- data.frame()
+	for (i in 1:dim(df_out)[1]){
+  		values <- t(as.data.frame(df_out$values[[i]]))
+  		rownames(values)<- c(df_out$code[[i]])
+		df_final <- rbind(df_final,values)
+	}
+	colnames(df_final) <- "Values"
+	ifelse(result_sim $status_code != 200,print("Error during calculation"),return(df_final))
+}
+
+
