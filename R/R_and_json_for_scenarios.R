98815935c378405ff2124f6e7e0026fd7bd6ffd3
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2014-03-13 10:58:23 +0100

    Working example in R

--- R/R_and_json_for_scenarios.R
+++ R/R_and_json_for_scenarios.R
@@ -1,47 +1,33 @@
 #####   0. Preparation  #####
-# On appelle des packages
+# On appelle des packages suivants. Attention: Il est important des les appeler dans cet ordre !!
+library(rjson)
 library(jsonlite)
 library(httr)
 
 #####   1. JSON to R  #####
 # On récupère un dictionnaire, par exemple 
 
-# json_data <-'{"scenarios": [{"foyers_fiscaux": [{"declarants": ["ind0"]}], "menages": [{"personne_de_reference": "ind0"}], "familles": [{"parents": ["ind0"]}], "year": 2013, "legislation_url": "http://api.openfisca.fr/api/1/default-legislation", "individus": [{"sali": 50000, "activite": "Actif occup\\u00e9", "statmarit": "C\\u00e9libataire", "cadre": true, "birth": "1970-01-01", "id": "ind0"}]}]}'
-# celib <- fromJSON(json_data)
 
-# Example with a scenario composed of a family with two kids and two parents
-json_couple <- '{"scenarios": [{"foyers_fiscaux": [{"personnes_a_charge": ["ind2", "ind3"],"declarants": ["ind0", "ind1"]}], "menages": [{"conjoint": "ind1", "enfants": ["ind2", "ind3"], "personne_de_reference": "ind0"}], "familles": [{"parents": ["ind0", "ind1"], "enfants": ["ind2", "ind3"]}], "year": 2013, "legislation_url": "http://api.openfisca.fr/api/1/default-legislation", "individus": [{"sali": 35000, "activite": "Actif occupé", "statmarit": "Marié", "birth": "1970-01-01", "id": "ind0"}, {"sali": 35000, "activite": "Actif occupé", "statmarit": "Marié", "birth": "1970-01-02", "id": "ind1"}, {"activite": "étudiant, élève", "id": "ind2", "birth": "2000-01-03"}, {"activite": "étudiant, élève", "id": "ind3", "birth": "2000-01-04"}]}]}'
+json_celib <- '{"scenarios": [{"axes": [{"count": 3, "max": 24000, "name": "sali", "min": 0}, {"count": 2, "max": 3000, "name": "loyer", "min": 1000}], "test_case": {"foyers_fiscaux": [{"declarants": ["ind0"]}], "individus": [{"cadre": true, "activite": "Actif occupé", "statmarit": "Célibataire", "id": "ind0", "birth": 1970}], "familles": [{"parents": ["ind0"]}], "menages": [{"personne_de_reference": "ind0"}]}, "year": 2013}], "decomposition": [{"code": "salsuperbrut", "title": "Salaire super brut"}, {"code": "salnet", "title": "Salaire net"}, {"code": "sali", "title": "Salaire imposable"}, {"code": "loyer", "title": "Loyer"}, {"code": "revdisp", "title": "Revenu disponible"}]}'
 
-couple <- fromJSON(json_data_couple)
-#                        
+celib <- fromJSON(json_celib)
 
-foyers_fiscaux <-as.data.frame(couple$scenarios$foyers_fiscaux)
-individus <-as.data.frame(couple$scenarios$individus)
-menages <-as.data.frame(couple$scenarios$menages)
-year <-as.data.frame(couple$scenarios$year)
+str(celib)
+str(celib$scenarios$axes)
+str(as.data.frame(celib$scenarios$test_case))
 
-individus
+foyers_fiscaux <-as.data.frame(celib$scenarios$test_case$foyers_fiscaux)
+individus <-as.data.frame(celib$scenarios$test_case$individus)
+menages <-as.data.frame(celib$scenarios$test_case$menages)
+year <-as.data.frame(celib$scenarios$test_case$year)
+
 
 # Get the results
-result_couple <- POST(url='http://api.openfisca.fr/api/1/simulate', body =json_couple,
+result_celib <- POST(url='http://api.openfisca.fr/api/1/simulate', body =json_celib,
           add_headers('Content-Type'= 'application/json',
                       'User-Agent'='OpenFisca-Notebook'))
-
-response = content(result_couple)
+fromJSON(result_celib)
+response = content(result_celib)
 str(response)
-str(response$value)
-str(response[5])
+as.data.frame(response$value)
 
-json_couple2 <- '{"scenarios": [{"foyers_fiscaux": [{"personnes_a_charge": ["ind2", "ind3"],"declarants": ["ind0", "ind1"]}], "menages": [{"conjoint": "ind1", "enfants": ["ind2", "ind3"], "personne_de_reference": "ind0"}], "familles": [{"parents": ["ind0", "ind1"], "enfants": ["ind2", "ind3"]}], "year": 2013, "legislation_url": "http://api.openfisca.fr/api/1/default-legislation", "individus": [{"sali": 35000, "activite": "Actif occupé", "statmarit": "Marié", "birth": "1970-01-01", "id": "ind0"}, {"sali": 35000, "activite": "Actif occupé", "statmarit": "Marié", "birth": "1970-01-02", "id": "ind1"}, {"activite": "étudiant, élève", "id": "ind2", "birth": "2000-01-03"}, {"activite": "étudiant, élève", "id": "ind3", "birth": "2000-01-04"}]}], "decomposition" : ["revdisp"]}'
-
-# Get the results
-result_couple2 <- POST(url='http://api.openfisca.fr/api/1/simulate', body =json_couple2,
-          add_headers('Content-Type'= 'application/json',
-                      'User-Agent'='OpenFisca-Notebook'))
-
-response2 = content(result_couple2)
-str(response2)
-
-
-
-#####   3. R to JSON  #####
